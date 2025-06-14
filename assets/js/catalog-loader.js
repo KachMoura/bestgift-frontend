d86e190e5301cebaecffc09682089d5737933bc2
KachMoura <kharkhach@gmail.com>
2025-06-14 23:54:57 +0200

    IA

--- assets/js/catalog-loader.js
+++ assets/js/catalog-loader.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     data-item-id="${id}"
                     data-item-name="${title}"
                     data-item-price="${price}"
-                    data-item-url="https://toto.html"
+                    data-item-url="https://www.bestgift.fr/product.html?id=${id}"
                     data-item-description="${description}"
                     data-item-image="${image}"
                     data-item-currency="eur"
